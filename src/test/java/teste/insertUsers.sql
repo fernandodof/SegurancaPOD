1760c7302bf668888bd7a0073c4824b2ac191830
filipegermano <filipegermano@outlook.com>
2014-05-29 17:07:20 -0300

    update manager, insert UnicastRemoteObject

---
+++ src/test/java/teste/insertUsers.sql
@@ -0,0 +1,24 @@
+create table users(
+	id serial,
+	login varchar(50),
+	password varchar(50),
+	primary key (id)
+)
+
+insert into users (login, password)values ('arigarcia', '123456');
+insert into users (login, password)values ('dudu', '123456');
+insert into users (login, password)values ('joao', '64527765');
+insert into users (login, password)values ('elis', '123567');
+insert into users (login, password)values ('samuel', '123456');
+insert into users (login, password)values ('fernan', '123456');
+insert into users (login, password)values ('magdidiel', '123456');
+insert into users (login, password)values ('kelsonn', '0987654');
+insert into users (login, password)values ('lulu', '0875543');
+insert into users (login, password)values ('filipe', 'jhv654');
+insert into users (login, password)values ('izabel', '8764432');
+insert into users (login, password)values ('maciel', 'ksrjhgf785');
+insert into users (login, password)values ('ezequiel', '27364587');
+insert into users (login, password)values ('wagner', '89274563');
+insert into users (login, password)values ('antoin', '2394866');
+insert into users (login, password)values ('joelanio', '123456');
+
