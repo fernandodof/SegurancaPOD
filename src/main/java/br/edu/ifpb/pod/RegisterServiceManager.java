61400037225dde345b52a561e1dff2c6d764ff92
filipegermano <filipegermano@outlook.com>
2014-05-29 14:50:18 -0300

    Create Service

---
+++ src/main/java/br/edu/ifpb/pod/RegisterServiceManager.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.pod;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author FILIPE
+ */
+public class RegisterServiceManager {
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+        Registry registry = LocateRegistry.createRegistry(10888);
+        registry.bind("LoginService", new Manager());
+                
+    }
+}
