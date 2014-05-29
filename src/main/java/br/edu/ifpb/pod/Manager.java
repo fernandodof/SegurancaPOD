ccee8698821b9d73f630814c7d618ba1ba516eed
fernandodof <fernandnodof@gmail.com>
2014-05-29 13:35:00 -0300

    Classe Manager

--- src/main/java/br/edu/ifpb/pod/Manager.java
+++ src/main/java/br/edu/ifpb/pod/Manager.java
@@ -7,28 +7,51 @@
 package br.edu.ifpb.pod;
 
 import br.edu.ifpb.dao.LoginDao;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import sun.security.jgss.LoginConfigImpl;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Fernando
  */
-public class Manager implements LoginService{
-
+public class Manager implements Serializable, LoginService{
+    LoginDao loginDao = new LoginDao();
+    ArrayList<Session> sessionList = new ArrayList();
+    
     @Override
     public Session login(String login, String password) {
-        LoginDao loginDao = new LoginDao();
-        loginDao.login(login, password)
+        Session session = null;
+        try {
+            if(this.loginDao.login(login, password)){
+                session = new Session();
+                sessionList.add(session);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Manager.class.getName()).log(Level.SEVERE, null, ex);
+            session = null;
+        }
+        return session;
     }
 
     @Override
     public void logout(Session sesion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        sessionList.remove(sesion);
     }
 
     @Override
     public Message sendMessage(Session session, Message message) {
+        if(sessionList.contains(session)){
+            return message;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean publish(ArrayList<Message> messages) throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
